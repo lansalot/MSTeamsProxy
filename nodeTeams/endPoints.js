b5825aac5c7fd41ed043fb28f801e54d5505a3bc
lansalot <lansalot@gmail.com>
2019-01-13 10:55:51 +0000

    Delete endPoints.js

--- nodeTeams/endPoints.js
+++
@@ -1,4 +0,0 @@
-var endPoints = {};
-endPoints["networkAlerts"] = "https://outlook.office.com/webhook/3bed0...5e";
-endPoints["serverAlerts"]  = "https://outlook.office.com/webhook/9ad34...5e";
-exports.endPoints = endPoints;
