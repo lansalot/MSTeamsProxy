1384f36c4e59b349fe91c543aeb193b0dfcc99ad
lansalot <lansalot@gmail.com>
2019-01-14 10:21:16 +0000

    Per reddit comments

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -1,21 +1,30 @@
+'use strict';
+
+const HttpsProxyAgent = require('https-proxy-agent');
+const request = require('request');
+const proxy = 'http://proxy:8080';
+const agent = new HttpsProxyAgent(proxy);
+const ep = require('../../endPoints.js');
+
 exports.PostToTeams = function (req, res) {
-    // {
-    //     "Title" : "Title goes here",
-    //     "Text" : "Summary text",
-    //     "Facts" : {
-    //         "Server Down": "terminate!"
-    //     }
-    // }
+    /*
+    {
+        "Title" : "Title goes here",
+        "Text" : "Summary text",
+        "Facts" : {
+            "Server Down": "terminate!"
+        }
+    }
+    */
 
     console.log(req.client.remoteAddress + ' ' + req.url);
-    var ep = require('../../endPoints.js');
     if (!ep.endPoints.hasOwnProperty(req.params.EndPoint)) {
         console.log('Endpoint ' + req.params.EndPoint + ' not found');
         res.sendStatus(404);
         return;
     }
 
-    var wintelTemplate = {
+    let WintelTemplate = {
         "@type": "MessageCard",
         "@context": "https://schema.org/extensions",
         "Summary": "nodebot",
@@ -26,19 +35,16 @@ exports.PostToTeams = function (req, res) {
             "Text": ""
         }]
     }
-    wintelTemplate.Title = req.body.Title;
-    wintelTemplate.sections[0].Text = req.body.Text;
-    for (var key in req.body.Facts) {
-        wintelTemplate.sections[0].Facts.push({
+    const { body: { Title, Text, Facts } } = req;
+    WintelTemplate.Title = Title;
+    WintelTemplate.sections[0].Text = Text;
+    for (let key in Facts) {
+        WintelTemplate.sections[0].Facts.push({
             "name": key,
-            "value": req.body.Facts[key]
+            "value": Facts[key]
         });
     }
 
-    var HttpsProxyAgent = require('https-proxy-agent');
-    var request = require('request');
-    var proxy = 'http://proxy:8080';
-    var agent = new HttpsProxyAgent(proxy);
     request({
             uri: ep.endPoints[req.params.EndPoint],
             method: "POST",
@@ -49,7 +55,7 @@ exports.PostToTeams = function (req, res) {
             timeout: 10000,
             followRedirect: true,
             maxRedirects: 10,
-            body: JSON.stringify(wintelTemplate)
+            body: JSON.stringify(WintelTemplate)
         },
         function (error, response, body) {
             //console.log("Error" + error);
@@ -65,18 +71,13 @@ exports.PostToTeamsPassThru = function (req, res) {
 
     console.log(req.client.remoteAddress + ' ' + req.url);
     console.log(req.body);
-    var ep = require('../../endPoints.js');
     if (!ep.endPoints.hasOwnProperty(req.params.EndPoint)) {
         console.log('Endpoint ' + req.params.EndPoint + ' not found');
         res.sendStatus(404);
         return;
     }
 
-    var passThru = req.body;
-    var HttpsProxyAgent = require('https-proxy-agent');
-    var request = require('request');
-    var proxy = 'http://proxy:8080';
-    var agent = new HttpsProxyAgent(proxy);
+    let passThru = req.body;
     request({
             uri: ep.endPoints[req.params.EndPoint],
             method: "POST",
@@ -88,7 +89,6 @@ exports.PostToTeamsPassThru = function (req, res) {
             followRedirect: true,
             maxRedirects: 10,
             body: JSON.stringify(passThru)
-            //body: passThru
         },
         function (error, response, body) {
             //console.log("Error" + error);
