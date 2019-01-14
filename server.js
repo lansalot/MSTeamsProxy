687a88ab4c51c4dff135f28da0d252f56654cc4d
lansalot <lansalot@gmail.com>
2019-01-14 10:20:35 +0000

    Changes as per reddit comments
    As per https://www.reddit.com/r/node/comments/afagbk/feedback_on_a_first_nodejs_project_please/

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-//https://stackoverflow.com/questions/8355473/listen-on-http-and-https-for-a-single-express-app
-var
+'use strict';
+// Proxy code found at:
+// https://stackoverflow.com/questions/8355473/listen-on-http-and-https-for-a-single-express-app
+
+const
   fs = require('fs'),
   express = require('express'),
   app = express(),
-  keys_dir = 'keys/'
+  keys_dir = 'keys/',
   https = require("https"),
   http = require("http"),
   server_options = {
     key : fs.readFileSync(keys_dir + 'teamsnodeprivkey.pem'),
-    ca  : fs.readFileSync(keys_dir + 'root.pem'),
+    ca  : fs.readFileSync(keys_dir + 'hcroot.pem'),
     cert: fs.readFileSync(keys_dir + 'teamsnodecert.pem')
-  }
+  };
 
-bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-var routes = require('./api/routes/TeamRoute');
+const routes = require('./api/routes/TeamRoute');
 routes(app);
 
 https.createServer(server_options,app).listen(3001);
